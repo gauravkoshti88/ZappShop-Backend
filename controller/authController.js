e975e2df5906d3bb288768871c24336324b6cbdd
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-05-07 01:21:07 +0530

    fix cors error

--- controller/authController.js
+++ controller/authController.js
@@ -38,8 +38,8 @@ export const register = async (req, res)=>{
 
         res.cookie('token', token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7*24*60*60*1000 // 7 days
         })
 
@@ -81,8 +81,8 @@ export const login = async (req, res)=>{
 
         res.cookie('token', token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7*24*60*60*1000 // 7 days
         })
 
@@ -130,8 +130,8 @@ export const googleLogin = async (req, res) =>{
         let token = await generateToken(user._id)
         res.cookie('token',token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
         })
         return res.status(200).json({
@@ -154,8 +154,8 @@ export const adminLogin = async(req, res)=>{
             let token = await generateToken1(email);
             res.cookie('token',token,{
                 httpOnly:true,
-                secure:false,
-                sameSite:"Strict",
+                secure:true,
+                sameSite:"none",
                 maxAge:1*24*60*60*1000
             })
             return res.status(200).json({
