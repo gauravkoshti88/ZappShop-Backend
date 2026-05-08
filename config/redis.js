1d38c160c2f7d19cbed5afe8b16585d01f214cf8
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-05-09 05:23:15 +0530

    Using redis database

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,8 @@
 import redis from "redis"
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL
+});
 
 redisClient.on("error", (err) => console.error("Redis Error", err));
 
